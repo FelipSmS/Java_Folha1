6a18513757ab95fbd92a3269495a660ce1e8443e
FelipeSmS <felpz.gg@gmail.com>
2024-03-18 20:01:46 -0300

    Atv4

---
+++ Folha1/Atv4.java
@@ -0,0 +1,31 @@
+//4. Escreva um algoritmo que leia dois números e ao final mostre a soma, subtração,
+//multiplicação e a divisão dos números lidos. 
+
+import java.util.Scanner;
+
+public class Atv4 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número: ");
+        int num1 = ler.nextInt();
+
+        System.out.println("Informe o segundo número: ");
+        int num2 = ler.nextInt();
+
+        int soma = num1 + num2;
+        int subtracao = num1 - num2;
+        int multiplicacao = num1 * num2;
+        int divisao = num1 / num2;
+
+        System.out.println("A soma desses dois números é: " + soma);
+        System.out.println("A subtracao desses dois números é: " + subtracao);
+        System.out.println("A multiplicação desses dois números é: " + multiplicacao);
+        System.out.println("A divisão desses dois números é: " + divisao);
+
+        ler.close();
+    }
+
+}
