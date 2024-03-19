da1f1b058270c812f8030e149e70aaf331f422d6
FelipeSmS <felpz.gg@gmail.com>
2024-03-18 21:00:55 -0300

    Atv20

---
+++ Folha1/Atv20.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Atv20 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite o tempo da viagem em horas: ");
+        double tempoViagemHoras = ler.nextDouble();
+
+        System.out.print("Digite a velocidade média da viagem em km/h: ");
+        double velocidadeMediaKmPorHora = ler.nextDouble();
+
+        double consumoPorLitro = 12; // km por litro
+        double distancia = tempoViagemHoras * velocidadeMediaKmPorHora;
+        double quantidadeLitros = distancia / consumoPorLitro;
+
+        System.out.println("Quantidade de litros de combustível gasta: " + quantidadeLitros);
+
+        ler.close();
+    }
+}
